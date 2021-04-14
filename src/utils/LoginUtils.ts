8b78ec97d5fa34748a113306fb75cd5ed2c90188
Abhinav Rastogi <abhinav.rastogi@harness.io>
2021-04-14 14:08:32 +0530

    feat: returnUrl support (#7)

--- src/utils/LoginUtils.ts
+++ src/utils/LoginUtils.ts
@@ -2,14 +2,40 @@ import type { User } from "services/portal";
 import AppStorage from "utils/AppStorage";
 
 export function handleLoginSuccess(resource?: User): void {
+  const queryString = window.location.hash?.split("?")?.[1];
+  const urlParams = new URLSearchParams(queryString);
+  const returnUrl = urlParams?.get("returnUrl");
+
   if (resource) {
     AppStorage.set("token", resource.token);
+    AppStorage.set("uuid", resource.uuid);
     AppStorage.set("acctId", resource.defaultAccountId);
     AppStorage.set("lastTokenSetTime", +new Date());
+
+    if (returnUrl) {
+      try {
+        const returnUrlObject = new URL(returnUrl);
+        // only redirect to same hostname
+        if (returnUrlObject.hostname === window.location.hostname) {
+          window.location.href = returnUrl;
+          return;
+        } else {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `"${returnUrl}" is a not a valid redirect due to hostname mismatch`
+          );
+        }
+      } catch (err) {
+        // returnUrl is not a valid url. do nothing.
+        // eslint-disable-next-line no-console
+        console.warn(`"${returnUrl}" is not a valid URL`);
+      }
+    }
 
     const experience = resource.accounts?.find(
       (account) => account.uuid === resource.defaultAccountId
     )?.defaultExperience;
+
     switch (experience) {
       case "NG":
         window.location.href = `/ng/#/account/${resource.defaultAccountId}/projects`;
@@ -19,5 +45,7 @@ export function handleLoginSuccess(resource?: User): void {
         window.location.href = `/#/account/${resource.defaultAccountId}/dashboard`;
         return;
     }
+  } else {
+    // TODO: handle empty login response
   }
 }
