f2f5cdf2d9d9eab51940f6dd33c3f401cfc87350
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-05-28 10:03:43 -0600

    fix: added a local site key for signup and fixed yarn.lock

--- src/pages/SignUp/Signup.tsx
+++ src/pages/SignUp/Signup.tsx
@@ -148,7 +148,10 @@ const SignUp: React.FC = () => {
               {passwordField}
               <ReCAPTCHA
                 ref={captchaRef}
-                sitekey={window.invisibleCaptchaToken || "1234"}
+                sitekey={
+                  window.invisibleCaptchaToken ||
+                  "6LfMgvcaAAAAAHCctQKV5AsCYZJHsKOpGH5oGc5Q" // site key for dev environments
+                }
                 size="invisible"
                 onChange={setCaptchaToken}
               />
