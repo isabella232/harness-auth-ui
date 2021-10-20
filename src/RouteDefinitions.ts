14fff78ee5132ba820963e3e2a45b4db2bbb2ec4
Sagnik De <sagnik.de@harness.io>
2021-10-20 10:54:40 -0400

    feat: [GTM-473]: Route removed for community edition

--- src/RouteDefinitions.ts
+++ src/RouteDefinitions.ts
@@ -6,7 +6,6 @@ export default {
   toResetPassword: () => `/reset-password/:token`,
   toTwoFactorAuth: () => `/two-factor-auth`,
   toSignUp: () => `/signup`,
-  toSignUpCommunity: () => `/signup/community`,
   toAcceptInvite: () => `/accept-invite`,
   toEmailVerification: () => `/email-verification`,
   toCompleteInvite: () => `/register/verify/:token`
