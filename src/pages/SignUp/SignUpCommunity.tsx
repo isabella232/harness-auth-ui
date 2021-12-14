644f000c8a1d94587a7da794140debc3d93c40b9
Sagnik De <sagnik.de@harness.io>
2021-12-14 18:00:01 -0500

    feat: [PLG-603]: singn up -> register for community (#94)
    * feat: [PLG-603]: Signup -> register
    
    * feat: [PLG-603]: Signup -> register
    
    * feat: [PLG-603]: More changes including a new link and other images
    
    * feat: [PLG-603]: More changes including a new link and other images

--- src/pages/SignUp/SignUpCommunity.tsx
+++ src/pages/SignUp/SignUpCommunity.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import cx from "classnames";
-import { Link } from "react-router-dom";
 import { Form } from "react-final-form";
 
 import BasicLayout from "components/BasicLayout/BasicLayout";
 
 import logo from "static/images/harness-logo.svg";
 import css from "./SignUp.module.css";
-import RouteDefinitions from "RouteDefinitions";
 import Field from "components/Field/Field";
 import { handleError } from "utils/ErrorUtils";
 import { validateEmail, validatePassword } from "utils/FormValidationUtils";
 import { useQueryParams } from "hooks/useQueryParams";
 import { SignupDTO, useCommunitySignup } from "../../services/ng";
 import { handleSignUpSuccess } from "../../utils/SignUpUtils";
+import { URLS } from "../../interfaces/OAuthProviders";
+import { Link } from "react-router-dom";
+import RouteDefinitions from "../../RouteDefinitions";
+import Text from "../../components/Text/Text";
 
 interface SignUpFormData {
   email: string;
@@ -85,9 +87,13 @@ const SignUpCommunity: React.FC = () => {
       <div className={cx(css.signup)}>
         <div className={css.header}>
           <img src={logo} width={120} className={css.logo} />
+          <div style={{ flex: 1 }} />
+          <Link to={RouteDefinitions.toSignIn()}>
+            <Text icon="leftArrow">Sign in</Text>
+          </Link>
         </div>
-        <div className={css.title}>Sign up</div>
-        <div className={css.subtitle}>and get ship done.</div>
+        <div className={css.title}>Welcome to Harness CD Community Plan</div>
+        <div className={css.communitySubtitle}>Set up an admin user</div>
         <Form
           onSubmit={onSubmit}
           render={({ handleSubmit }) => (
@@ -99,16 +105,32 @@ const SignUpCommunity: React.FC = () => {
               {passwordField}
               <input
                 type="submit"
-                value="Sign up"
+                value="Create user"
                 className="button primary"
                 disabled={loading}
               />
             </form>
           )}
         />
-        <div className={css.footer}>
-          Already have an account?{" "}
-          <Link to={RouteDefinitions.toSignIn()}>Sign in</Link>
+        <div className={cx(css.disclaimer)}>
+          By registering, you agree to our&nbsp;
+          <a
+            className={css.externalLink}
+            href={URLS.PRIVACY_AGREEMENT}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Privacy Policy&nbsp;
+          </a>
+          and our&nbsp;
+          <a
+            className={css.externalLink}
+            href={URLS.SUBSCRIPTION_TERMS}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Terms of Use
+          </a>
         </div>
       </div>
     </BasicLayout>
